542d19bafff152296509bc5688b5a4e920be6a20
joy <jalmeenkaursethi@gmail.com>
2017-10-15 17:47:09 +0530

    Add two numbers

---
+++ src/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//TODO : Helper Class
+    //TODO : Varun - Do it by command line arguments
+    //TODO : Puneet - Do it by method overloading
+    //TODO : Yogesh - Do it by making simple add method
+
+      helpervarun h1= new helpervarun();
+
+      h1.add();
+    }
+}
